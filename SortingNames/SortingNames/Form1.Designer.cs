8da75d3251f45b012b7da7a864f0c0f8384819af
refelia <rherlihy20@gmail.com>
2018-05-05 00:54:09 -0400

    Adding ClassAssignment

---
+++ SortingNames/SortingNames/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SortingNames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortAZToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.countTextBox = new System.Windows.Forms.TextBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(372, 388);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(75, 23);
+            this.loadBtn.TabIndex = 1;
+            this.loadBtn.Text = "Load ";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 381);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(470, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sortAZToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // sortAZToolStripMenuItem
+            // 
+            this.sortAZToolStripMenuItem.Name = "sortAZToolStripMenuItem";
+            this.sortAZToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.sortAZToolStripMenuItem.Text = "Sort A-Z";
+            this.sortAZToolStripMenuItem.Click += new System.EventHandler(this.sortAZToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(281, 298);
+            this.timeTextBox.Multiline = true;
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.ReadOnly = true;
+            this.timeTextBox.Size = new System.Drawing.Size(166, 58);
+            this.timeTextBox.TabIndex = 5;
+            // 
+            // countTextBox
+            // 
+            this.countTextBox.Location = new System.Drawing.Point(281, 196);
+            this.countTextBox.Multiline = true;
+            this.countTextBox.Name = "countTextBox";
+            this.countTextBox.ReadOnly = true;
+            this.countTextBox.Size = new System.Drawing.Size(166, 58);
+            this.countTextBox.TabIndex = 6;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(281, 39);
+            this.searchTextBox.Multiline = true;
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(166, 76);
+            this.searchTextBox.TabIndex = 7;
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(372, 130);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 8;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(281, 130);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(75, 23);
+            this.resetBtn.TabIndex = 9;
+            this.resetBtn.Text = "Clear";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 435);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.countTextBox);
+            this.Controls.Add(this.timeTextBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.loadBtn);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sortAZToolStripMenuItem;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.TextBox countTextBox;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button resetBtn;
+    }
+}
+
---
+++ SortingNames/SortingNames/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SortingNames
+{
+    public partial class Form1 : Form
+    {
+        const int SIZE = 5000;
+
+        //Declare a string array.
+        string[] countArray = new string[SIZE];
+
+        // Pass the file path and file name to the StreamReader constructor
+        StreamReader strReader = new StreamReader("Names.csv"); 
+        string ListOfName = string.Empty;
+
+        int index;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        { }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+           
+            DateTime start = DateTime.Now;
+            double end = (DateTime.Now - start).TotalSeconds;
+            try
+            {
+                //Read the first line of text
+                ListOfName = strReader.ReadLine();
+                int index = 0;
+
+                //Continue to read until you reach end of file
+                while (ListOfName != null)
+                {
+                    countArray[index] = ListOfName.ToString();
+                    this.listBox1.Items.Add(ListOfName);
+                    //Read the next line
+                    ListOfName = strReader.ReadLine();
+                    // start incrementing
+                    index++;
+                    //Display the the total number of items in the listbox
+                    countTextBox.Text = listBox1.Items.Count.ToString() + ", " + " Names found in : " + " " + (end.ToString()) + " " +
+                         " Seconds";
+                }
+             
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+           
+        }
+        //Create a method selectionSort.
+        public void SelectionSort(string[] countArray)
+        {
+            //declare varable
+            int lowestIndex = 0 ;
+            string lowestValue ;
+
+            for (int searchValue = 0; searchValue <countArray.Length; searchValue++)
+            {
+                lowestValue = countArray[searchValue];
+                lowestIndex = searchValue;
+
+                for (int index = searchValue + 1; index < countArray.Length; index++)
+                {
+                    if (string.Compare(lowestValue, countArray[index], true) == 1)
+                    {
+                        lowestIndex = index;
+                        lowestValue = countArray[index];
+                    }
+                }
+                swap(ref countArray[lowestIndex], ref countArray[searchValue]);
+            }
+
+        }
+        public void swap(ref string smallestValue, ref string biggestValue)
+        {
+            if (smallestValue != biggestValue)
+            {
+                string temp = smallestValue;
+                smallestValue = biggestValue;
+                biggestValue = temp;
+            }
+        }
+        private void label2_Click(object sender, EventArgs e)
+        { }
+
+        private void sortAZToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Clear all the text in the listbox before start sorting. 
+                listBox1.Items.Clear();
+
+                //Declare a datetime variable and initialize with the Datetime.Now value
+                DateTime start = DateTime.Now;
+                double end = (DateTime.Now - start).TotalSeconds;
+
+
+                while (index < countArray.Length && !strReader.EndOfStream)
+                {
+                    countArray[index] = strReader.ReadLine();
+                    index++;
+                }
+
+                listBox1.Items.Clear();
+                SelectionSort(countArray);
+
+                foreach (string value in countArray)
+                {
+                    listBox1.Items.Add(value);
+                }
+                timeTextBox.Text = listBox1.Items.Count.ToString()
+                + " , " + "name found! SORTING TIME:  " + " " + (end.ToString()) + " " + "Seconds";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string pathFinder = "sortedNames.csv";
+            StreamWriter strWriter = new StreamWriter(pathFinder);
+            
+            foreach(string listOfNames in listBox1.Items)
+            {
+                //write a text file.
+                strWriter.WriteLine(listOfNames.ToString());
+
+                //Display message
+                MessageBox.Show("Exported!");
+            }
+        }
+        private int binarySearch(string[] countArray, string value)
+        {
+            //Declare a variable 
+                int leftFromMidPoint = 0;
+                int rightFromMidPoint = countArray.Length - 1;
+                int midPoint;
+                int possition = -1;
+                bool found = false;
+
+                //While loop to search for the value.
+                while (!found && leftFromMidPoint <= rightFromMidPoint)
+                {
+                    //Assigning value to the midpoint of array
+                    midPoint = (leftFromMidPoint + rightFromMidPoint) / 2;
+
+                    //if found
+                    if (countArray[midPoint] == value)
+                    {
+                        found = true;
+                        possition = midPoint;
+                    }
+                    // if the value is in the lefside of the midpoint 
+                    else if (string.Compare(countArray[midPoint], value, false) > 0)
+                    {
+                        rightFromMidPoint = midPoint - 1;
+                    }
+                    //if the value is in the right side of the midpoint
+                    else
+                    {
+                        leftFromMidPoint = midPoint + 1;
+                    }
+                }
+                return possition;
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Exit the program.
+            Application.Exit();
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               //Declare variable of the time
+                DateTime start = DateTime.Now;
+                double end = (DateTime.Now - start).TotalSeconds;
+
+                //Decalre the variable and initialize with value
+                string name = searchTextBox.Text;
+                int position = binarySearch(countArray, searchTextBox.Text);
+                bool found = countArray.Contains(name);
+
+                //Checking if the statement is true.
+                if(position >= 0)
+                {
+                    found = true;
+                }
+                // if  false then ...
+                if(found == false)
+                {
+                    //display "name does not exist"
+                    searchTextBox.Text = " Name does not exist!";
+                }
+
+                //Checking if the statement is true
+              listBox1.SetSelected(position, true);
+
+                //declare a string text and initialized with value
+                string text = listBox1.GetItemText(listBox1.SelectedItem);
+
+                //Display the name and index location and time that it took for scanning.
+                searchTextBox.Text = text + " " + " NAME FOUND!" +  " AT INDEX LOCATION: " + position + " " + 
+                                     " SCANNING TIME : "  + (end.ToString()) + " " + "seconds";
+            }
+            catch (Exception)
+            {
+                //Display the error message.
+                MessageBox.Show("Name does not exist");
+            }
+        }
+
+        private void resetBtn_Click_1(object sender, EventArgs e)
+        {
+            //Clear all the text in the search textbox.
+            searchTextBox.Text = "";
+
+            //Set the focus.
+            searchTextBox.Focus();
+        }
+    }
+    
+}
+
+
+
+
+    
+
